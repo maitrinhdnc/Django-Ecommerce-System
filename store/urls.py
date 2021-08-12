8e5e2f3b9fcbfce48d210442b7b9cea2956feea5
maitrinhdnc <maitrinhdnc@gmail.com>
2021-08-12 15:23:22 +0700

    first upload full project

---
+++ store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('cart/', views.cart, name='cart'),
+    path('update_item/', views.updateItem, name='update_item'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('process_order/', views.processOrder, name='process_order'),
+
+]
